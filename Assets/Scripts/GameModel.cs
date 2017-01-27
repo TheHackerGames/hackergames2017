0194954b29f7c762c1f67a7559f18103a8e71605
Andrzej Korolczuk <Andrzej.Korolczuk@king.com>
2017-01-27 12:04:01 +0000

    collecting shit

---
+++ Assets/Scripts/GameModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameModel : MonoBehaviour {
+
+	public int NumSonarChargers = 3;
+	public int Score;
+
+	private List<Collectible> collectibles = new List<Collectible>();
+
+	void Start () {
+
+		collectibles = GameObject.FindObjectsOfType<Collectible>().ToList();
+		Debug.Log(GameObject.FindObjectsOfType<Collectible>().Length);
+	}
+
+	public void Collect(Collectible collectible)
+	{
+		collectibles.Remove(collectible);
+		Score += collectible.Score;
+	}
+
+	public int NumCollectiblesRemaining
+	{
+		get 
+		{
+			return collectibles.Count;
+		}
+	}
+}
---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+	[SerializeField]
+	private Text sonarsText;
+
+	[SerializeField]
+	private Text collectiblesRemaining;
+
+
+	[SerializeField]
+	private Text scoreText;
+
+	[SerializeField]
+	private GameModel gameModel;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		sonarsText.text = string.Format("Sonars: {0}", gameModel.NumSonarChargers);
+		collectiblesRemaining.text = string.Format("Items remaining: {0}", gameModel.NumCollectiblesRemaining);
+		scoreText.text = string.Format("Score: {0}", gameModel.Score);
+
+	}
+}
